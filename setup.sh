a05cc079cc892f23f8448a4103c832f981820962
Alberto Planas <aplanas@gmail.com>
2014-10-20 11:51:29 +0200

    Initial commit.

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# virtualenv env
+# . ./env/bin/activate
+
+pip install -U numpy scipy
+pip install -U matplotlib
+
+pip install -U git+https://github.com/scikit-learn/scikit-learn.git#egg=scikit-learn
+
+pip install -U ipython[all]
+
+pip install -U https://pypi.python.org/packages/source/G/GitPython/GitPython-0.3.2.RC1.tar.gz
