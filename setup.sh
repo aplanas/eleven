c3068244a22893cdc0c15aef3e2d0164e4f65b22
Alberto Planas <aplanas@gmail.com>
2014-10-20 14:31:25 +0200

    Add patch information.

--- setup.sh
+++ setup.sh
@@ -11,4 +11,9 @@ pip install -U git+https://github.com/scikit-learn/scikit-learn.git#egg=scikit-l
 
 pip install -U ipython[all]
 
-pip install -U https://pypi.python.org/packages/source/G/GitPython/GitPython-0.3.2.RC1.tar.gz
+pip install -U git+https://github.com/gitpython-developers/gitdb.git#egg=gitdb
+
+# https://pypi.python.org/packages/source/G/GitPython/GitPython-0.3.2.RC1.tar.gz
+pip install -U git+https://github.com/gitpython-developers/GitPython.git#egg=GitPython
+
+# Apply https://github.com/johnsca/GitPython/commit/db82455bd91ce00c22f6ee2b0dc622f117f07137
