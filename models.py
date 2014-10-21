bc6405fb288499540aed71af81f668e056e65d26
Alberto Planas <aplanas@gmail.com>
2014-10-21 17:37:31 +0200

    Use SliceFeature to build a cascade of features.

--- models.py
+++ models.py
@@ -6,14 +6,14 @@ from pprint import pprint
 from time import time
 
 import numpy as np
-from scipy import sparse
-from sklearn import preprocessing
+from sklearn.base import BaseEstimator
 from sklearn.datasets.base import Bunch
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.linear_model import SGDClassifier
 from sklearn.grid_search import GridSearchCV
+from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.preprocessing import LabelEncoder, Normalizer
 
 
 def fetch_gitlog(data_path, stats=False):
@@ -33,7 +33,7 @@ def fetch_gitlog(data_path, stats=False):
 
         # Encode targets into numbers
         target = [d[6] for d in data]
-        le = preprocessing.LabelEncoder()
+        le = LabelEncoder()
         le.fit(target)
         target_names = le.classes_
         target = le.transform(target)
@@ -50,6 +50,51 @@ def fetch_gitlog(data_path, stats=False):
                  DESCR=description)
 
 
+class SliceFeature(BaseEstimator):
+    """Estimator to slice a feature."""
+
+    def __init__(self, slc, astype=None):
+        """Build an instance using a slice object.
+
+        >>> X = np.array([[1, 2, 3], [10, 20, 30]])
+        >>> X
+        array([[ 1,  2,  3],
+               [10, 20, 30]])
+
+        >>> slc = SliceFeature(slice(0, 1))
+        >>> slc.transform(X)
+        array([ 1, 10])
+
+        >>> slc = SliceFeature(slice(0, 2))
+        >>> slc.transform(X)
+        array([[ 1,  2],
+               [10, 20]])
+
+        """
+        self.slc = slc
+        self.astype = astype
+
+    def fit(self, X, y=None):
+        """Does nothing: this transformer is stateless."""
+        return self
+
+    def transform(self, X, y=None):
+        """Does nothing: this transformer is stateless."""
+        if self.slc.step:
+            index = range(self.slc.start, self.slc.stop, self.slc.step)
+        else:
+            index = range(self.slc.start, self.slc.stop)
+
+        result = X[:, index]
+        if len(index) == 1:
+            result = result.reshape(X.shape[0])
+
+        if self.astype:
+            result = result.astype(self.astype)
+
+        return result
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train different models with the same dataset.')
     parser.add_argument('-c', '--csv', help='csv file name')
@@ -63,30 +108,59 @@ if __name__ == '__main__':
     data = fetch_gitlog(args.csv, stats=True)
 
     pipeline_summary = Pipeline([
+        ('slice', SliceFeature(slice(0, 1))),
         ('vect', CountVectorizer()),
         ('tfidf', TfidfTransformer()),
     ])
 
     pipeline_message = Pipeline([
+        ('slice', SliceFeature(slice(1, 2))),
         ('vect', CountVectorizer()),
         ('tfidf', TfidfTransformer()),
     ])
 
-    l = data.data.shape[0]
-    features_summary = pipeline_summary.fit_transform(data.data[:, [0]].reshape(l))
-    features_message = pipeline_message.fit_transform(data.data[:, [1]].reshape(l))
-    features_numeric = data.data[:, [2, 3, 4, 5]].astype(int)
-    print 'SP1', features_summary.shape
-    # print 'SP2', features_message.shape
-    # print 'SP3', features_numeric.shape
-    features = sparse.hstack((features_summary, features_message, features_numeric))
+    pipeline_numeric = Pipeline([
+        ('slice', SliceFeature(slice(2, 6), astype=int)),
+    ])
 
-    print 'SPF', features.shape
+    main_pipeline = Pipeline([
+        ('features', FeatureUnion([
+            ('summary', pipeline_summary),
+            ('message', pipeline_message),
+            ('numeric', pipeline_numeric),
+        ])),
+        ('normalizer', Normalizer()),
+        ('clf', SGDClassifier()),
+    ])
 
-    classifier = SGDClassifier()
-    classifier.fit(features, data.target)
+    parameters = {
+        'features__summary__vect__max_df': (0.5, 0.75, 1.0),
+        'features__summary__vect__max_features': (None, 5000, 10000, 50000),
+        'features__summary__vect__ngram_range': ((1, 1), (1, 2)),  # unigrams or bigrams
+        'features__summary__tfidf__use_idf': (True, False),
+        'features__summary__tfidf__norm': ('l1', 'l2'),
+        'features__message__vect__max_df': (0.5, 0.75, 1.0),
+        'features__message__vect__max_features': (None, 5000, 10000, 50000),
+        'features__message__vect__ngram_range': ((1, 1), (1, 2)),  # unigrams or bigrams
+        'features__message__tfidf__use_idf': (True, False),
+        'features__message__tfidf__norm': ('l1', 'l2'),
+        'clf__alpha': (0.00001, 0.000001),
+        'clf__penalty': ('l2', 'elasticnet'),
+        'clf__n_iter': (10, 50, 80),
+    }
+    grid_search = GridSearchCV(main_pipeline, parameters, n_jobs=-1, verbose=1)
 
-    print classifier.predict(features) - data.target
-    print data.target
+    print 'Performing grid search...'
+    print 'pipeline:', [name for name, _ in main_pipeline.steps]
+    print 'parameters:'
+    pprint(parameters)
+    t0 = time()
+    grid_search.fit(data.data, data.target)
+    print 'done in %0.3fs' % (time() - t0)
+    print
 
-    # print features.getrow(10)
+    print 'Best score: %0.3f' % grid_search.best_score_
+    print 'Best parameters set:'
+    best_parameters = grid_search.best_estimator_.get_params()
+    for param_name in sorted(parameters.keys()):
+        print '\t%s: %r' % (param_name, best_parameters[param_name])
