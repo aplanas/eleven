2752ffe3939b1d933c0b295d27b5631dc4a5ca84
Alberto Planas <aplanas@gmail.com>
2014-10-21 10:53:08 +0200

    PEP8 cleaning.

--- repo-log.py
+++ repo-log.py
@@ -8,12 +8,12 @@ import git
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract git history information.')
-    parser.add_argument('-f' , '--from',           dest = 'from_', help='from revno')
-    parser.add_argument('-t' , '--to',             help = 'to revno')
-    parser.add_argument('-l' , '--limit',          help = 'max number of commits')
-    parser.add_argument('-p' , '--project',        help = 'project directory')
-    parser.add_argument('-r' , '--git-repository', dest = 'project',         help='project directory')
-    parser.add_argument('-c' , '--csv',            help = 'csv file name')
+    parser.add_argument('-f', '--from', dest='from_', help='from revno')
+    parser.add_argument('-t', '--to', help='to revno')
+    parser.add_argument('-l', '--limit', help='max number of commits')
+    parser.add_argument('-p', '--project', help='project directory')
+    parser.add_argument('-r', '--git-repository', dest='project', help='project directory')
+    parser.add_argument('-c', '--csv', help='csv file name')
 
     args = parser.parse_args()
 
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     if not args.to and not args.limit:
         parser.print_help()
         exit(1)
-    
+
     with open(args.csv, 'w') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', doublequote=True)
         repo = git.Repo(args.project)
