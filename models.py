a6c41c018b3495c95b42b6b0a4ca56d1f47e0cf8
Alberto Planas <aplanas@gmail.com>
2014-10-21 10:19:40 +0200

    Initial draft for models.py.

---
+++ models.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+import argparse
+import csv
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train different models with the same dataset.')
+    parser.add_argument('-c', '--csv', help='csv file name')
+
+    args = parser.parse_args()
+
+    if not args.csv:
+        parser.print_help()
+        exit(1)
+
+    with open(args.csv, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"',
+                               doublequote=True)
+        next(csvreader)
+        # Summary, Message, Number of Files, Total Lines, Added Lines, Deleted Lines, Label, SHA-1
+        full_dataset = [(line[0].strip(), line[1].strip(),
+                         int(line[2]), int(line[3]), int(line[4]),
+                         int(line[5]), line[6], line[7]) for line in csvreader]
+
+        dataset = [d for d in full_dataset if d[-2]]
+        print 'Original dataset [%d]' % len(full_dataset)
+        print 'Labeled dataset [%d]' % len(dataset)
+        print 'Ratio [%2.2f%%]' % (100.0 * len(dataset) / len(full_dataset))
+
+        # scipy.sparse.hstack((X_train, csr_matrix(f1).T), format='csr').todense()
