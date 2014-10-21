dda2798146574976d4caff1a3b7d9393939a71ce
Alberto Planas <aplanas@gmail.com>
2014-10-21 10:49:54 +0200

    Refactor fetc_gitlog

--- models.py
+++ models.py
@@ -4,6 +4,27 @@ import argparse
 import csv
 
 
+def fetch_gitlog(data_path, stats=False):
+    """Convert the CSV log into a datase suitable for scikit-learn."""
+    with open(data_path, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"',
+                               doublequote=True)
+        next(csvreader)
+        # Summary, Message, Number of Files, Total Lines, Added Lines, Deleted Lines, Label, SHA-1
+        full_dataset = [(line[0].strip(), line[1].strip(),
+                         int(line[2]), int(line[3]), int(line[4]),
+                         int(line[5]), line[6], line[7]) for line in csvreader]
+
+        dataset = [d for d in full_dataset if d[-2]]
+
+        if stats:
+            print 'Original dataset [%d]' % len(full_dataset)
+            print 'Labeled dataset [%d]' % len(dataset)
+            print 'Ratio [%2.2f%%]' % (100.0 * len(dataset) / len(full_dataset))
+
+    return dataset
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train different models with the same dataset.')
     parser.add_argument('-c', '--csv', help='csv file name')
@@ -14,18 +35,4 @@ if __name__ == '__main__':
         parser.print_help()
         exit(1)
 
-    with open(args.csv, 'rb') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"',
-                               doublequote=True)
-        next(csvreader)
-        # Summary, Message, Number of Files, Total Lines, Added Lines, Deleted Lines, Label, SHA-1
-        full_dataset = [(line[0].strip(), line[1].strip(),
-                         int(line[2]), int(line[3]), int(line[4]),
-                         int(line[5]), line[6], line[7]) for line in csvreader]
-
-        dataset = [d for d in full_dataset if d[-2]]
-        print 'Original dataset [%d]' % len(full_dataset)
-        print 'Labeled dataset [%d]' % len(dataset)
-        print 'Ratio [%2.2f%%]' % (100.0 * len(dataset) / len(full_dataset))
-
-        # scipy.sparse.hstack((X_train, csr_matrix(f1).T), format='csr').todense()
+    dataset = fetch_gitlog(args.csv, stats=True)
