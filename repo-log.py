126cb8ca259c697059aa7718e3f4354b4ff37bd5
Susanne Oberhauser <froh@suse.com>
2014-10-21 10:32:15 +0200

    repo-log.py: - add --git-repo as alias for --project - allow tag names as both --from and --to

--- repo-log.py
+++ repo-log.py
@@ -8,11 +8,12 @@ import git
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract git history information.')
-    parser.add_argument('-f', '--from', dest='from_', help='from revno')
-    parser.add_argument('-t', '--to', help='to revno')
-    parser.add_argument('-l', '--limit', help='max number of commits')
-    parser.add_argument('-p', '--project', help='project directory')
-    parser.add_argument('-c', '--csv', help='csv file name')
+    parser.add_argument('-f' , '--from',           dest = 'from_', help='from revno')
+    parser.add_argument('-t' , '--to',             help = 'to revno')
+    parser.add_argument('-l' , '--limit',          help = 'max number of commits')
+    parser.add_argument('-p' , '--project',        help = 'project directory')
+    parser.add_argument('-r' , '--git-repository', dest = 'project',         help='project directory')
+    parser.add_argument('-c' , '--csv',            help = 'csv file name')
 
     args = parser.parse_args()
 
@@ -20,13 +21,22 @@ if __name__ == '__main__':
         parser.print_help()
         exit(1)
 
+    if not args.to and not args.limit:
+        parser.print_help()
+        exit(1)
+    
     with open(args.csv, 'w') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', doublequote=True)
         repo = git.Repo(args.project)
+
+        if args.to:
+            args.to = repo.commit(args.to).hexsha
+
         if args.limit:
             iter_ = repo.iter_commits(args.from_, max_count=args.limit, no_merges=True)
         else:
             iter_ = repo.iter_commits(args.from_, no_merges=True)
+
         for commit in iter_:
             if commit.hexsha == args.to:
                 break
